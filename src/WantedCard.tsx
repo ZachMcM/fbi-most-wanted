3e131c4b567bd07b5b8bcba6d4ecc67bdd20ae0a
Zach McMullen <zachmcmullen04@gmail.com>
2023-03-06 23:51:20 -0800

    broken image function

--- src/WantedCard.tsx
+++ src/WantedCard.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react"
 
 export default function WantedCard(wantedInfo: any) {
-    const [modalState, setModalState] = useState<string>('hidden')
-
     const { cauton, description, person_classification, reward, hair, eyes, height_min, height_max, age_range, title, url, image } = wantedInfo
     
     function getHeight(num: number) {
@@ -14,7 +12,7 @@ export default function WantedCard(wantedInfo: any) {
     return (
         <div className='w-full p-10 mb-7 lg:mb-0 shadow-xl rounded-lg'>
             <div className="flex flex-wrap items-center">
-                <img onClick={() => setModalState('block')} className="w-16 h-16 rounded-full ring-4 ring-white mr-5 hover:opacity-80 cursor-pointer mb-5 lg:mb-none" src={image} />
+                <img className="w-16 h-16 rounded-full ring-4 ring-white m-5 lg:mb-none" src={image} />
                 <a href={url} className="text-sky-white hover:underline text-lg">{title}</a>
             </div>
             {person_classification && person_classification !== 'Main' &&
@@ -47,15 +45,6 @@ export default function WantedCard(wantedInfo: any) {
                     <p className="text-gray-400 text-sm mt-5">Reward: {reward}</p>
                 } 
             </ul>
-
-            <div onClick={() => setModalState('hidden')} className={`${modalState} fixed top-0 left-0 right-0 z-50 w-full p-36 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full bg-cover flex justify-center items-center cursor-pointer`}>
-                <div className="absolute top-0 right-0 p-10">
-                    <i onClick={() => setModalState('hidden')} className="fa-solid fa-xmark text-2xl cursor-pointer hover:text-gray-400"></i>
-                </div>
-                <div className="p-24">
-                    <img className="rounded-xl cursor-default" src={image}></img>
-                </div>
-            </div>
         </div>
     )
 }
