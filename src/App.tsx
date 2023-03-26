8da21dd8a312b12c98d9b1fba98f5f247189b901
Zach McMullen <zachmcmullen04@gmail.com>
2023-03-26 02:06:01 -0700

    footer

--- src/App.tsx
+++ src/App.tsx
@@ -99,16 +99,6 @@ export default function App() {
         </section>
 
       </div>
-      <footer className='p-10 bg-gray-900'>
-            <ul className='flex text-sm my-2'>
-                <li className='mx-1'><a className='text-sky-400 hover:text-sky-500' href='https://github.com/ZachMcM'>Github</a></li>
-                <li className='mx-1 text-sky-400'>·</li>
-                <li className='mx-1'><a className='text-sky-400 hover:text-sky-500' href='https://www.linkedin.com/in/zach-mcmullen-ba3a2621b/'>LinkedIn</a></li>
-                <li className='mx-1 text-sky-400'>·</li>
-                <li className='mx-1'><a className='text-sky-400 hover:text-sky-500' href='mailto:zachmcmullen04@gmail.com'>Email</a></li>
-            </ul>
-            <p className='text-gray-400 text-sm ml-1'>Made with <i className="fa-solid fa-heart"></i> by <a className='text-sky-400 hover:text-sky-500 cursor-pointer' href='https://zachmcmullen.com'>Zach McMullen</a></p>
-        </footer>
     </>
   )
 }
